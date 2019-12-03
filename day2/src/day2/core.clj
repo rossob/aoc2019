5217cb67e8c61061ae393f4be0b01b53ee254bd5
Ross Blumhorst <rblumhorst@armsbusinesssolutions.com>
2019-12-03 12:48:16 -0600

    day 2 part 2 working - RB

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -4,6 +4,7 @@
   (:gen-class))
 
 (def filename "day2pt1input.txt")
+(def goal-output 19690720)
 
 ;; read intcode string to vector of ints
 (defn get-cpu []
@@ -11,8 +12,12 @@
 
 ;; stated in problem for setup -
 ;; replace position 1 with the value 12 and replace position 2 with the value 2
-(defn set-alarm-state [cpu]
-  (assoc (assoc cpu 2 2) 1 12))
+(defn set-alarm-state [cpu x y]
+  (assoc (assoc cpu 2 y) 1 x))
+
+;; calculate program alarm from given inputs (100 * x + y)
+(defn calc-program-alarm [x y]
+  (+ (* 100 x) y))
 
 ;; set value at n+3 using values at n+1 and n+2
 (defn process-cpu [cpu pos process-fn]
@@ -29,5 +34,20 @@
       99 (nth cpu 0)
       "def" (str "something went wrong - pos " pos ", str " (nth cpu pos)))))
 
+;; loop through all different x-y pairs comparing against constant goal output
+;; return either calculated program alarm value or 0 if goal not found
+(defn find-program-alarm-val []
+  (let [original-cpu (get-cpu)]
+  (loop [x 0
+         y 0]
+      (if (= (process-opcode (set-alarm-state original-cpu x y)) goal-output)
+        (calc-program-alarm x y)
+        (if (= y 99)
+          (if (= x 99)
+            0
+            (recur (+ x 1) 0))
+          (recur x (+ y 1)))))))
+
+
 ;; get cpu, set up, process, then display first value
-(defn -main [] (println (process-opcode (set-alarm-state (get-cpu)))))
+(defn -main [] (println (find-program-alarm-val)))
